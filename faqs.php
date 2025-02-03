e668f2d3ee2c6fefa8d76ae22166f84300268b99
piedracita <arnaldpedros@gmail.com>
2025-02-03 23:31:57 +0100

    update nav pendnet

---
+++ faqs.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+
+<html lang="es">
+
+    <head>
+        <!-- COMMON-HEAD -->
+        <?php include "common-php/head.html"; ?>
+        <!-- /COMMON-HEAD -->
+
+        <!-- CANONICAL -->
+	    <link rel="canonical" href="https://www.saxofonista.cat/faqs.php">
+
+        <!-- COMMON-LOCALIZATION -->
+        <?php include "common-php/localization/main.html"; ?>
+        <!-- /COMMON-LOCALIZATION -->
+
+        <title>Preguntas, Disponibilidad y Precios | Saxofonista para bodas y eventos | SAXCAT</title>
+
+        <meta name="description" content="Saxofonista para bodas, contrata música en directo para tu boda. Disponible en Barcelona, Madrid, Costa Brava, Sitges y más">
+        <meta name="keywords" content="saxo, saxofonista, contratar, bodas, música en directo, barcelona, costa brava">
+        <meta name="author" content="SAXCAT">
+
+        <!-- COMMON-OPENGRAPH -->
+        <?php include "common-php/opengraph/es.html"; ?>
+        <!-- /COMMON-OPENGRAPH -->
+        
+    </head>
+
+    <body>
+
+        <!-- COMMON-HEADER -->
+        <?php include "common-php/header/es.html"; ?>
+        <!-- /COMMON-HEADER -->
+
+        <!-- BANNER -->
+        <section id="slider" class="hero p-0 odd featured">
+            <div class="swiper-container no-slider animation slider-h-50 slider-h-auto">
+                <div class="swiper-wrapper">
+
+                    <!-- Item 1 -->
+                    <div class="swiper-slide slide-center">
+
+                        <!-- Media -->
+                        <video class="full-image to-bottom" data-mask="70" playsinline autoplay muted loop>
+                            <source src="assets/videos/work.mp4" type="video/mp4" />
+                        </video> 
+
+                        <div class="slide-content row text-center">
+                            <div class="col-12 mx-auto inner">
+
+                                <!-- Content -->
+                                <!--<nav data-aos="zoom-out-up" data-aos-delay="800" aria-label="breadcrumb">
+                                    <ol class="breadcrumb">
+                                        <li class="breadcrumb-item"><a href="/">Inicio</a></li>
+                                        <li class="breadcrumb-item active" aria-current="page">Saxofonista para Bodas</li>
+                                    </ol>
+                                </nav>-->
+                                <h1 class="mb-0 title effect-static-text">Saxofonsita para bodas</h1>
+                                <p>Si estás buscando incorporar música en directo a tu boda y te gustaría contar con saxofonista, tanto si es como músico solista o en grupo, estás en el lugar indicado. A continuación te detallamos las opciones más solicitadas para añadir música en directo en saxofón para tu celebración.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+        <!-- /BANNER -->
+
+
+        <!-- FAQS -->
+        <section id="faqs" class="section-3 odd">
+            <div class="container smaller">
+                <div class="row intro">
+                    <div class="col-12">
+                        <h2>FAQS</h2>
+                        <p>¿Tenéis preguntas o dudas sobre la posibilidad de contratar un saxofonista para vuestro evento especial, boda o evento corporativo? A continuación os detallo las respuectas a algunas de las preguntas que más me hacen antes de contratarme como saxofonista. La preparación de ciertos eventos requiere de atención a los detalles, si tenéis alguna duda que no os quede resuelta ¡no tardéis en escribirme! Será un placer ayudaros.</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 col-md-6">
+                        <p>¿Cual es el precio de contratar un saxofonista?</p>
+                        <blockquote>El precio varia en función del lugar y del tipo de evento, pudiendo haber descuentos en fechas puntuales o gastos extra por desplazamiento y similares. Un precio aproximado es 550€ (impuestos no incluidos).</blockquote>
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <p>¿Que estilo de música interpretas?</p>
+                        <blockquote>¿Tenéis preguntas o dudas sobre la posibilidad de contratar un saxofonista para vuestro evento especial, boda o evento corporativo? A continuación os detallo las respuectas a algunas de las preguntas que más me hacen antes de contratarme como saxofonista. La preparación de ciertos eventos requiere de atención a los detalles, si tenéis alguna duda que no os quede resuelta ¡no tardéis en escribirme! Será un placer ayudaros.</blockquote>
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <p>¿Cuales son los plazos de pago?</p>
+                        <blockquote>Para la contratación es imprescindible abonar un cierto porcentaje del total como depósito, el resto se puede pagar hasta la misma fecha del evento.</blockquote>
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <p>¿Te desplazas para eventos?</p>
+                        <blockquote>Sí, por supuesto! Lo más frecuente es encontrarme actuando en grandes ciudades como Barcelona, Madrid o Valencia, sin embargo actúo en toda España y también en el extrangero.</blockquote>
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <p>¿Quien es el mejor saxofonista?</p>
+                        <blockquote>He trabajado con muchos saxofonistas y cada uno le da un toque especial, me gusta también ayudar a mis amigos músicos por lo que si me llega una petición concreta en que un saxofonista en que confíe pueda dar el mejor servicio posible, estaré encantado de recomendarlo.</blockquote>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- /FAQS -->
+
+        <!-- COMMON-CONTACT -->
+        <?php include "common-php/contact/es.html"; ?>
+        <!-- /COMMON-CONTACT -->
+        
+        <!-- COMMON-FOOTER -->
+        <?php include "common-php/footer/es.html"; ?>
+        <!-- /COMMON-FOOTER --> 
+
+        <!-- COMMON-BODY-SCRIPTS -->
+        <?php include "common-php/body-scripts.html"; ?>
+        <!-- /COMMON-BODY-SCRIPTS -->
+    </body>
+</html>
---
+++ fotos.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+
+<html lang="es">
+
+    <head>
+        <!-- COMMON-HEAD -->
+        <?php include "common-php/head.html"; ?>
+        <!-- /COMMON-HEAD -->
+
+        <!-- CANONICAL -->
+	    <link rel="canonical" href="https://www.saxofonista.cat/fotos.php">
+
+        <!-- COMMON-LOCALIZATION -->
+        <?php include "common-php/localization/main.html"; ?>
+        <!-- /COMMON-LOCALIZATION -->
+
+        <title>Fotos | Saxofonista para bodas | Contratar Saxo Profesional | SAXCAT</title>
+
+        <meta name="description" content="Fotos de saxofonistas para bodas, grabados en Barcelona, Madrid y lugares únicos de España.">
+        <meta name="keywords" content="saxo, saxofonista, fotos, bodas, barcelona, madrid, españa">
+        <meta name="author" content="SAXCAT">
+
+        <!-- COMMON-OPENGRAPH -->
+        <?php include "common-php/opengraph/es.html"; ?>
+        <!-- /COMMON-OPENGRAPH -->
+        
+    </head>
+
+    <body>
+
+        <!-- COMMON-HEADER -->
+        <?php include "common-php/header/es.html"; ?>
+        <!-- /COMMON-HEADER -->
+
+        <!-- BANNER -->
+        <section id="slider" class="hero p-0 odd featured">
+            <div class="swiper-container no-slider animation slider-h-50 slider-h-auto">
+                <div class="swiper-wrapper">
+
+                    <!-- Item 1 -->
+                    <div class="swiper-slide slide-center">
+
+                        <!-- Media -->
+                        <video class="full-image to-bottom" data-mask="70" playsinline autoplay muted loop>
+                            <source src="assets/videos/work.mp4" type="video/mp4" />
+                        </video> 
+
+                        <div class="slide-content row text-center">
+                            <div class="col-12 mx-auto inner">
+
+                                <!-- Content -->
+                                <!--<nav data-aos="zoom-out-up" data-aos-delay="800" aria-label="breadcrumb">
+                                    <ol class="breadcrumb">
+                                        <li class="breadcrumb-item"><a href="/">Inicio</a></li>
+                                        <li class="breadcrumb-item active" aria-current="page">Saxofonista para Bodas</li>
+                                    </ol>
+                                </nav>-->
+                                <h1 class="mb-0 title effect-static-text">Fotos de saxofonista en eventos</h1>
+                                <p>Fotos de los mejores saxofonistas para bodas y eventos, disponibles para contratación, en estilo moderno o jazz, actuando como solistas, junto al DJ en formato DJ SAXO y en grupo de música para aportar música en directo en tu boda o evento.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+        <!-- /BANNER -->
+
+
+        <!-- GALLERY -->
+        <section id="gallery" class="section-2 offers">
+            <div class="container">
+                <div class="row text-center intro">
+                    <div class="col-12">
+                        <h2>Fotos</h2>
+                        <p>Algunas de mis fotos favoritas actuando como saxofonsita en eventos y bodas.</p>
+                    </div>
+                </div>
+                <div class="row gallery justify-content-center items">
+                    <a class="col-12 col-md-6 col-lg-4 item" href="assets/images/gallery/saxophonist-wedding-cocktail-01.jpg">
+                        <img loading="lazy" src="assets/images/gallery/saxophonist-wedding-cocktail-01.jpg" alt="Project" class="w-100">
+                    </a>
+                    <a class="col-12 col-md-6 col-lg-4 item" href="assets/images/gallery/saxo-eventos-01.jpg">
+                        <img loading="lazy" src="assets/images/gallery/saxo-eventos-01.jpg" alt="Project" class="w-100">
+                    </a>
+                    <a class="col-12 col-md-6 col-lg-4 item" href="assets/images/gallery/events-sax-01.jpg">
+                        <img loading="lazy" src="assets/images/gallery/events-sax-01.jpg" alt="Project" class="w-100">
+                    </a>
+                    <a class="col-12 col-md-6 col-lg-4 item" href="assets/images/gallery/electronic-sax-01.jpg">
+                        <img loading="lazy" src="assets/images/gallery/electronic-sax-01.jpg" alt="Project" class="w-100">
+                    </a>
+                    <a class="col-12 col-md-6 col-lg-4 item" href="assets/images/gallery/saxo-bodas-01.jpg">
+                        <img loading="lazy" src="assets/images/gallery/saxo-bodas-01.jpg" alt="Project" class="w-100">
+                    </a>
+                    <a class="col-12 col-md-6 col-lg-4 item" href="assets/images/gallery/wedding-sax-01.jpg">
+                        <img loading="lazy" src="assets/images/gallery/wedding-sax-01.jpg" alt="Project" class="w-100">
+                    </a>
+                </div>
+            </div>
+        </section>
+        <!-- /GALLERY -->
+
+        <!-- COMMON-CONTACT -->
+        <?php include "common-php/contact/es.html"; ?>
+        <!-- /COMMON-CONTACT -->
+        
+        <!-- COMMON-FOOTER -->
+        <?php include "common-php/footer/es.html"; ?>
+        <!-- /COMMON-FOOTER --> 
+
+        <!-- COMMON-BODY-SCRIPTS -->
+        <?php include "common-php/body-scripts.html"; ?>
+        <!-- /COMMON-BODY-SCRIPTS -->
+    </body>
+</html>
